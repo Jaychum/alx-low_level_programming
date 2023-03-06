dccbf85348b23d017c8e9684439cf85e20b0682b
Jaychum <chiomaogbo5@gmail.com>
2023-03-06 09:33:12 -0800

    combination 2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 /**
  * main - main function
@@ -13,18 +12,18 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 98; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 1; j <= 98; j++)
+		for (j = 0; j < 100; j++)
 		{
-			if (0 < 0)
+			if (i < j)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
 				putchar(' ');
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
-				if (i == 98 && j == 99)
+				if (i != 98 && j != '0')
 				{
 					putchar(',');
 					putchar(' ');
