08ca5868ee32b12a55f2b0f95bb8f080be7a5c68
Jaychum <chiomaogbo5@gmail.com>
2023-03-16 11:50:51 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,32 +5,25 @@
  * @ptr: pointer
  * @old_size: old size
  * @new_size: new size
- * Return: pointer
+ * Return: ptr
+ * if new_size == old_size, returns ptr without changes
+ * if malloc fails, returns NULL.
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *realloc;
-	unsigned int i;
-
-	if (ptr != NULL)
-		clone = ptr;
-	else
-	{
-		return (malloc(new_size));
-	}
-	if (new_size == old_size)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (0);
+		return (NULL);
 	}
-	realloc = malloc(new_size);
-	if (realloc == NULL)
-		return (0);
-	for (i = 0; i < (old_size || i < new_size); i++)
-	{
-		*(realloc + i) = clone[i];
-	}
+	if (ptr == NULL)
+	       ptr = malloc(new_size);
+
+	if (new_size == old_size)
+		return (ptr);
+
 	free(ptr);
-	return (realloc);
+	ptr = malloc(new_size);
+	return (ptr);
 }
